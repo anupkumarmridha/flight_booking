8ccb9f426482a82df60c47a72adbeb19e61eb112
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-02-02 12:49:48 +0530

    updated: admin filter

--- home/admin.py
+++ home/admin.py
@@ -17,10 +17,17 @@ class SeatAdmin(admin.ModelAdmin):
     list_filter=('flight_class','flight__name',)
     
 class RouteAdmin(admin.ModelAdmin):
-    list_filter=('arrival_location','departure_location','travel_time')
+    list_filter=('arrival_location','departure_location','travel_time',)
     
+class StopAdmin(admin.ModelAdmin):
+    search_fields=('location',)
+    list_filter=('route__departure_location','route__arrival_location','route__travel_time','arrival_time', 'departure_time','duration',)
+class ScheduleAdmin(admin.ModelAdmin):
+    search_fields=('flight__name', 'flight__flight_no', 'route__departure_location', 'route__arrival_location',)
+    list_filter=('flight__name', 'flight__flight_no','route__departure_location','route__arrival_location','total_available_seats_on_flight','route__travel_time','arrival_time', 'departure_time','duration',)
+
 admin.site.register(Flight, FlightAdmin)
 admin.site.register(Route, RouteAdmin)
-admin.site.register(Stop)
-admin.site.register(Schedule)
+admin.site.register(Stop, StopAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Seat, SeatAdmin)
