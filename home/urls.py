eaea27bf1f6404e7e734e4ab2a1aa69dd307ee85
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-02-03 02:21:39 +0530

    added:search feature

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.homeView, name="homeView"),
     path("all_routes/", views.AllRoutes, name="all_routes"),
     path("all_schedules/<int:pk>", views.allSchedules, name="all_schedules"),
+    path('search/', views.search, name='search'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Schedule, Route
-
+from django.db.models import Q
+from datetime import datetime
 # Create your views here.
 def homeView(request):
     routes = Route.objects.all()
@@ -28,3 +29,41 @@ def allSchedules(request, pk):
     }
     return render(request, "home/all_schedules.html", context)
 
+def search(request):
+    query = request.GET.get('q')
+    start_date_str = request.GET.get('start_date')
+    end_date_str = request.GET.get('end_date')
+    source_location = request.GET.get('source_location')
+    destination_location = request.GET.get('destination_location')
+
+    try:
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date() if start_date_str else None
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date() if end_date_str else None
+    except ValueError:
+        start_date, end_date = None, None
+
+    schedules = Schedule.objects.all()
+    filters = Q()
+    if start_date:
+        filters &= Q(source_departure_datetime__date__gte=start_date)
+    if end_date:
+        filters &= Q(source_departure_datetime__date__lte=end_date)
+    if source_location:
+        filters &= Q(route__departure_location__icontains=source_location)
+    if destination_location:
+        filters &= Q(route__arrival_location__icontains=destination_location)
+    if query:
+        filters &= Q(route__name__icontains=query)
+
+    schedules = schedules.filter(filters).order_by("source_departure_datetime")
+
+    context = {
+        "AllSchedules": schedules,
+        "query": query,
+        "start_date": start_date_str,
+        "end_date": end_date_str,
+        "source_location": source_location,
+        "destination_location": destination_location,
+    }
+
+    return render(request, "home/index.html", context)
