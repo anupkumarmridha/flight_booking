76be34ceb6e2a04dbd30e89a7fefc1153ce82ea7
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-02-03 18:51:45 +0530

    update: env config

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -162,8 +162,7 @@ GRAPH_MODELS = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = env('EMAIL_BACKEND')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
