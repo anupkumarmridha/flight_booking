dc2d70fbaf53c1c5a677ef3809abed3c365315a9
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-02-04 23:26:44 +0530

    added: admin filter for booking

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from booking.models import Booking, Payment
 
 # Register your models here.
-admin.site.register(Booking)
-admin.site.register(Payment)
+class BookingAdmin(admin.ModelAdmin):
+    search_fields=('schedule', 'status', 'travel_date', 'source_location',)
+    list_filter=('user', 'travel_date', 'source_location','destination_location','schedule', 'status','amount','total_seats','seats', )
+class PaymentAdmin(admin.ModelAdmin):
+    search_fields=('booking', 'status', 'amount',)
+    list_filter=('booking', 'status', 'created_at', 'amount', 'method',)
+
+
+admin.site.register(Booking, BookingAdmin)
+admin.site.register(Payment, PaymentAdmin)
 # admin.site.register(Cancellation)
