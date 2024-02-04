da63ab201e09b0cfed19743925590e0ae0ad1567
anupkumarmridha <anupkumarmridha.net@gmail.com>
2024-02-04 17:40:43 +0530

    fix:static file problem

--- flight_booking/settings.py
+++ flight_booking/settings.py
@@ -130,14 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = "media/"
+STATIC_URL = os.path.join(BASE_DIR, "static")
+MEDIA_URL = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # STATIC_ROOT = '/django/static/'
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # added manually
 
