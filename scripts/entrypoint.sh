b43c3edad39a3f161fdede8c3c50666d19d56ddc
Anup Kumar Mridha <66965208+anupkumarmridha@users.noreply.github.com>
2024-02-11 00:28:49 +0530

    Update entrypoint.sh for debuging

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-set -e
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsuperuser --noinput --username=admin --email=admin@gmail.com --password=admin
-
+echo "Collecting static files..."
+python manage.py collectstatic --noinput || { echo "Failed to collect static files"; exit 1; }
+echo "Making migrations..."
+python manage.py makemigrations || { echo "Failed to make migrations"; exit 1; }
+echo "Applying migrations..."
+python manage.py migrate || { echo "Failed to apply migrations"; exit 1; }
+echo "Creating superuser..."
+python manage.py createsuperuser --noinput --username=admin --email=admin@gmail.com --password=admin || { echo "Failed to create superuser"; exit 1; }
 exec "$@"
